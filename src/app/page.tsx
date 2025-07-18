7741de0052eda549da75ecbf8045f297e4ffa5f6
Paula Suyane <paulasuyaneprojeto2023@gmail.com>
2025-07-18 10:31:50 -0300

    ajuste largura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ProductList from '@/components/ProductList'
 export default function Home() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className="max-w-5xl mx-auto px-4 py-7">
+      <div className="max-w-7xl mx-auto px-4 py-7">
         <h1 className="text-4xl font-bold text-center text-gray-800 mb-1">
           Cardápio <span className="text-rose-600">Um Sushi</span> 🍣
         </h1>
